ff53ca13668b6d4e620b639f1ebdeb2a4f226b59
Bryan Harrell <bryan@lifeofbrybry.com>
2016-01-03 18:24:52 +0100

    Added Heroku suitable build pack

--- PythonCMS/settings.py
+++ PythonCMS/settings.py
@@ -77,14 +77,25 @@ WSGI_APPLICATION = 'PythonCMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #     'NAME': 'PythonCMS',
+    #    'USER': 'bryan',
+     #   'PASSWORD': 'diablos3',
+      #  'HOST': '',
+       # 'PORT': '',
+    #}
+#}
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
